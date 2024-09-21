9bb722158ac6dd4a5c7ff9631f862635193286fe
Mark Bumanglag <markjehn@hawaii.edu>
2024-09-21 11:46:08 -1000

    I created a dropdown menu in html. I added css and also js in app.js.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { getAllTodos, createTodo } from './firebase/features/todo';
 import { applyModalChanges, closeModal } from './modal';
 
+
 const modalCloseButton = document.getElementById('cancel_modal_button');
 const modalApplyButton = document.getElementById('apply_modal_button');
 
@@ -22,3 +23,33 @@ const handleAddTask = async () => {
 
 modalCloseButton.addEventListener('click', closeModal);
 modalApplyButton.addEventListener('click', handleAddTask);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdowns = document.querySelectorAll('.dropdown');
+  dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.select');
+  const caret = dropdown.querySelector('.caret');
+  const menu = dropdown.querySelector('.menu');
+  const options = dropdown.querySelectorAll('.menu li'); 
+  const selected = dropdown.querySelector('.selected');
+
+  select.addEventListener('click', () => {
+      select.classList.toggle('select-clicked');
+      caret.classList.toggle('caret-rotate');
+      menu.classList.toggle('menu-open');
+  });
+
+  options.forEach(option => {
+      option.addEventListener('click', () => {
+      selected.innerText = option.innerText;
+      select.classList.remove('select-clicked');
+      caret.classList.remove('caret-rotate');
+      menu.classList.remove('menu-open');
+      options.forEach(opt => {
+          opt.classList.remove('active'); 
+      });
+      option.classList.add('active'); 
+      });
+  });
+  });
+})
